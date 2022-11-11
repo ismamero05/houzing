0144b876b181fccb45f2537a9abddf1c8099a085
ismamero05 <ismailovmuhammadyusuf3@gmail.com>
2022-11-12 04:46:56 +0500

    project configured

---
+++ src/context/properties/index.jsx
@@ -0,0 +1,13 @@
+import { createContext, useReducer } from "react";
+import { reducer } from "./reducer";
+
+const PropertiesContext = createContext();
+
+export const PropertiesProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, [])
+    return (
+        <PropertiesContext.Provider value={[state, dispatch]}>
+            {children}
+        </PropertiesContext.Provider>
+    )
+}
