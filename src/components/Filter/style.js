a9846372e38a786e3cb3b45b5053e4ea12736fd4
ismamero05 <ismailovmuhammadyusuf3@gmail.com>
2022-11-20 15:09:15 +0500

    filter finished

--- src/components/Filter/style.js
+++ src/components/Filter/style.js
@@ -1,44 +1,55 @@
-import styled from "styled-components";
-import {ReactComponent as houses} from '../../assets/icons/houses.svg'
-import {ReactComponent as search} from '../../assets/icons/search.svg'
-import {ReactComponent as settingLines} from '../../assets/icons/setting-lines.svg'
+import styled from 'styled-components';
+import { ReactComponent as houses } from '../../assets/icons/houses.svg';
+import {ReactComponent as filter} from '../../assets/icons/setting-lines.svg'
+import { ReactComponent as search } from '../../assets/icons/search.svg';
+import { Select } from 'antd';
 
 const Container = styled.div`
-    display: flex;
-    padding: var(--padding);
-    padding-top: 10px;
-    padding-bottom: 10px;
-    gap: 20px;
+  display: flex;
+  padding: var(--padding);
+  padding-top: 10px;
+  padding-bottom: 10px;
+  gap: 20px;
+  width: 100%;
+  max-width: 1440px;
+  margin: auto;
+`;
 
-    width: 100%;
-    max-width: 1440px;
-    margin: auto;
-`
-
-const Icons = styled.div``
-
-Icons.Houses = styled(houses)`
-    margin-right: 8px;
-`
+const Icons = styled.div``;
 
 Icons.Search = styled(search)`
-    margin-right: 8px;
-`
-
-Icons.SettingLines = styled(settingLines)`
-    margin-right: 8px;
-`
+  margin-right: 8px;
+`;
+Icons.Filter = styled(filter)`
+  margin-right: 8px;
+`;
+Icons.Houses = styled(houses)`
+  margin-right: 8px;
+`;
 
 const MenuWrapper = styled.div`
-    padding: 30px;
-    background-color: white;
-    border: 1px solid black;
-`
-
+  padding: 30px;
+  background: white;
+  border: 1px solid #e6e9ec;
+`;
 const Section = styled.div`
-    margin-bottom: 20px;
-    display: flex;
-    gap: 20px;
-`
+  display: flex;
+  justify-content: start;
+  gap: 20px;
+  margin-bottom: 20px;
+`;
 
-export { Container, Icons, MenuWrapper, Section }
+const SelectAnt = styled(Select)`
+  min-width: 200px;
+  width: 100%;
+  max-width: 200px;
+  .ant-select-selector {
+    height: 44px !important;
+  }
+  .ant-select-selection-item {
+    display: flex !important;
+    align-items: center !important;
+  }
+`;
+
+export { Container, Icons, MenuWrapper, Section, SelectAnt };
